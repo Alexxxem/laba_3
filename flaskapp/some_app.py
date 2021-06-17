d561768061a4ac3ec74746b54401788867584998
Alexxxem <85002081+Alexxxem@users.noreply.github.com>
2021-06-17 22:02:09 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -9,8 +9,9 @@ def hello():
 
 from flask import render_template
 from flask_wtf import FlaskForm,RecaptchaField
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
+from wtforms.validators import AnyOf
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 SECRET_KEY = 'secret'
 app.config['SECRET_KEY'] = SECRET_KEY
@@ -24,18 +25,16 @@ from flask_bootstrap import Bootstrap
 bootstrap = Bootstrap(app)
 # создаем форму для загрузки файла
 class NetForm(FlaskForm):
-    # поле для введения строки, валидируется наличием данных
-    # валидатор проверяет введение данных после нажатия кнопки submit
-    # и указывает пользователю ввести данные если они не введены
-    # или неверны
-    size = StringField('size', validators = [DataRequired()])
     # поле загрузки файла
     # здесь валидатор укажет ввести правильные файлы
     upload = FileField('Load image', validators=[
     FileRequired(),
     FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
-    # поле формы с capture
     recaptcha = RecaptchaField()
+    red = StringField('First color', validators=[DataRequired(),AnyOf(['red','green', 'blue'],'red green blue only!')])
+    green = StringField('Second color', validators=[DataRequired(),AnyOf(['red','green', 'blue'],'red green blue only!')])
+    blue = StringField('Third color', validators=[DataRequired(),AnyOf(['red','green', 'blue'],'red green blue only!')])
+
     #кнопка submit, для пользователя отображена как send
     submit = SubmitField('send')
 # функция обработки запросов на адрес 127.0.0.1:5000/net
@@ -49,60 +48,94 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import seaborn as sns
 ## функция для оброботки изображения
-def draw(filename,size):
+def draw(filename,red,green,blue):
     ##открываем изображение
     print(filename)
     img= Image.open(filename)
-    ##делаем график
-    fig = plt.figure(figsize=(6, 4))
-    ax = fig.add_subplot()
-    data = np.random.randint(0, 255, (100, 100))
-    ax.imshow(img, cmap='plasma')
-    b = ax.pcolormesh(data, edgecolors='black', cmap='plasma')
-    fig.colorbar(b, ax=ax)
-    gr_path = "./static/newgr.png"
-    sns.displot(data)
-    #plt.show()
-    plt.savefig(gr_path)
-    plt.close()
-    #рисуем рамки
-    size=int(size)
     height = 224
     width = 224
     img= np.array(img.resize((height,width)))/255.0
-    print(size)
-    img[:size,:,1] = 0
-    img[:,0:size,1] = 0
-    img[:,224-size:,1] = 0
-    img[224-size:,:,1] = 0
-    ##сохраняем новое изображение
-    img = Image.fromarray((img * 255).astype(np.uint8))
-    print(img)
-    #img = Image.fromarray(img)
-    new_path = "./static/new.png"
-    print(img)
-    img.save(new_path)
-    return new_path, gr_path
+    ##делаем график
+    from skimage import io
+    _ = plt.hist(img.ravel(), bins = 256, color = 'orange')
+    _ = plt.hist(img[:,:,0].ravel(), bins = 256, color = 'Red', alpha = 0.5)
+    _ = plt.hist(img[:,:,1].ravel(), bins = 256, color = 'Green', alpha = 0.5)
+    _ = plt.hist(img[:,:,2].ravel(), bins = 256, color = 'Blue', alpha = 0.5)
+    _ = plt.xlabel('Intensity Value')
+    _ = plt.ylabel('Count')
+    _ = plt.legend(['Total','Red_Channel', 'Green_Channel', 'Blue_Channel'])
 
-# метод обработки запроса GET и POST от клиента
+    plt_img2=np.zeros((224,20,3))
+    plt_img2[:,0,:]=(np.average(img,(0))).astype(np.float)
+    for i in range (1,19):
+        plt_img2[:,i,:]= plt_img2[:,0,:]
+    plt_img2 = Image.fromarray((plt_img2*255).astype(np.uint8))
+
+    plt_img=np.zeros((20,224,3))
+    plt_img[0,:,:]=(np.average(img,(1))).astype(np.float)
+    for i in range (1,19):
+        plt_img[i,:,:]= plt_img[0,:,:]
+    plt_img = Image.fromarray((plt_img2*255).astype(np.uint8))
+
+    gr_path = "./static/newgr.png"
+    plt.savefig(gr_path)
+    plt.close()
+    gr_path_horiz = "./static/horizgr.png"
+    plt_img.save(gr_path_horiz)
+    gr_path_vert = "./static/vertgr.png"
+    plt_img2.save(gr_path_vert)
+
+    result_img=np.zeros((224,224,3))
+    if red == 'red':
+        result_img[:,:,0] = img[:,:,0]
+    if red == 'green':
+        result_img[:,:,0] = img[:,:,1]
+    if red == 'blue':
+        result_img[:,:,0] = img[:,:,2]
+
+    if green == 'red':
+        result_img[:,:,1] = img[:,:,0]
+    if green == 'green':
+        result_img[:,:,1] = img[:,:,1]
+    if green == 'blue':
+        result_img[:,:,1] = img[:,:,2]
+
+    if blue == 'red':
+        result_img[:,:,2] = img[:,:,0]
+    if blue == 'green':
+        result_img[:,:,2] = img[:,:,1]
+    if blue == 'blue':
+        result_img[:,:,2] = img[:,:,2]
+
+    result_img = Image.fromarray((img*255).astype(np.uint8))
+    new_path = "./static/new.png"
+    img = Image.fromarray((img*255).astype(np.uint8))
+    old_path = "./static/old.png"
+    img.save(old_path)
+    result_img.save(new_path)
+    return new_path, gr_path, old_path, gr_path_vert, gr_path_horiz
+
 @app.route("/net",methods=['GET', 'POST'])
 def net():
-    # создаем объект формы
-    form = NetForm()
-    # обнуляем переменные передаваемые в форму
+    form = NetForm(meta={'csrf': False})
     filename =None
     newfilename=None
     grname=None
+    oldimgname=None
+    vertgr_name=None
+    horizgr_name=None
     # проверяем нажатие сабмит и валидацию введенных данных
     if form.validate_on_submit():
         # файлы с изображениями читаются из каталога static
         filename = os.path.join('./static', secure_filename(form.upload.data.filename))
-        sz=form.size.data
+        red=form.red.data
+        green=form.green.data
+        blue=form.blue.data
         form.upload.data.save(filename)
-        newfilename, grname = draw(filename,sz)
+        newfilename, grname, oldimgname, vertgr_name, horizgr_name = draw(filename,red,green,blue)
         # передаем форму в шаблон, так же передаем имя файла и результат работы нейронной
         # сети если был нажат сабмит, либо передадим falsy значения
-    return render_template('net.html',form=form,image_name=newfilename,gr_name=grname)
+    return render_template('net.html',form=form,image_name=newfilename,gr_name=grname, old_img=oldimgname,vertgr=vertgr_name,horizgr=horizgr_name)
 if __name__ == "__main__":
     app.run(host='127.0.0.1',port=5000)
 
