8376dbcb421c84517a398bee6233f7d27b6bf32b
Alexxxem <85002081+Alexxxem@users.noreply.github.com>
2021-06-18 13:31:53 +0700

    Update net.py

--- flaskapp/net.py
+++ flaskapp/net.py
@@ -1,50 +1,33 @@
-import random
-# бибилиотека keras для НС
-import keras
-# входной слой сети и модель сети
-from keras.layers import Input
-from keras.models import Model
-# одна из предобученных сетей
-from keras.applications import MobileNetV2
-from keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
 import os
 # модуль работы с изображениями
 from PIL import Image
 import numpy as np
-height = 224
+import sys
+import os
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import random
+
+def red_image(image_):
+    image_ = image_.sum(axis=2)/3.0 
+    image_new = np.zeros((224,224,3))
+    image_new [:,:,0]= image_ 
+    return image_new
+
+image = Image.open('static/123.jpg')
+
+'''height = 224
 width = 224
-nh=224
-nw=224
-ncol=3
-# загружаем и создаем стандартную уже обученную сеть keras
-visible2 = Input(shape=(nh,nw,ncol),name = 'imginp')
-resnet = MobileNetV2(include_top=True,
-weights="imagenet", input_tensor=visible2,
-input_shape=None, pooling=None, classes=1000)
-# чтение изображений из каталога
-# учтите, если там есть файлы не соответствующие изображениям или каталоги
-# возникнет ошибка
-def read_image_files(files_max_count,dir_name):
-    files = [item.name for item in os.scandir(dir_name) if item.is_file()]
-    files_count = files_max_count
-    if(files_max_count>len(files)): # определяем количество файлов не больше max
-        files_count = len(files)
-    image_box = [[]]*files_count
-    for file_i in range(files_count): # читаем изображения в список
-        image_box[file_i] = Image.open(dir_name+'/'+files[file_i]) # / ??
-    return files_count, image_box
-# возвращаем результаты работы нейронной сети
-def getresult(image_box):
-    files_count = len(image_box)
-    images_resized = [[]]*files_count
-    # нормализуем изображения и преобразуем в numpy
-    for i in range(files_count):
-        images_resized[i] = np.array(image_box[i].resize((height,width)))
-    images_resized = np.array(images_resized)
-    images_resized = preprocess_input(images_resized)
-    # подаем на вход сети изображение в виде numpy массивов
-    out_net = resnet.predict(images_resized)
-    # декодируем ответ сети в один распознанный класс top=1 (можно больше классов)
-    decode = decode_predictions(out_net, top=1)
-    return decode
+images_resized = np.array(image.resize(height,width))/255.0
+images_resized = np.array(images_resized)'''
+image1 = np.array(image)
+
+fig_ = plt.figure(figsize=(15,15))
+viewer = fig_.add_subplot(1,1,1)
+
+image_ = image1.copy()
+viewer.imshow(image1)
+
+plt.show()
  
